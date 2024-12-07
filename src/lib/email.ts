189e7a80fcc9ccaf4821348afe791c7ccb049e8c
Nicolas Siciliano <nhsiciliano@MacBook-Air-de-Nicolas.local>
2024-12-07 18:01:54 -0300

    secure:false

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'production') {
     transporter = nodemailer.createTransport({
         host: process.env.EMAIL_SERVER_HOST,
         port: Number(process.env.EMAIL_SERVER_PORT),
-        secure: true, // Usar SSL/TLS
+        secure: false, // Usar SSL/TLS
         auth: {
             user: process.env.EMAIL_SERVER_USER,
             pass: process.env.EMAIL_SERVER_PASSWORD,
